a0c13aaf5ebbfce32cc526fb4d5029d7b740a2b4
Fabien Delpierre <fab.delpierre@gmail.com>
2023-12-14 22:10:22 +0100

    retry fix bug responsive navigation

--- assets/style/app.js
+++ assets/style/app.js
@@ -1,4 +1,4 @@
-// (function ($) {
+//
 //     // a retravailler
 //     $(window).resize(function () {
 //         if ($(window).width() > 1077) {
@@ -30,50 +30,49 @@
 //         $('#navbar-toggle')[0].classList.remove('active');
 //     });
 
-// })(jQuery);
+//
+(function ($) {
+    const burger = document.getElementById('navbar-toggle');
+    const links = document.querySelectorAll('#link');
+    const nav = document.querySelectorAll('nav ul li');
+    const navComponent = document.querySelectorAll('nav ul li a');
+    const hidden = document.querySelector('.hidden');
+    let isOpen = false;
+    let playOnce = true;
 
-const burger = document.getElementById('navbar-toggle');
-const links = document.querySelectorAll('#link');
-const nav = document.querySelectorAll('nav ul li');
-const navComponent = document.querySelectorAll('nav ul li a');
-const hidden = document.querySelector('.hidden');
-let isOpen = false;
-let playOnce = true;
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        $('nav ul li').toggle('slow');
+    });
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    $('nav ul li').toggle('slow');
-});
-
-hidden.addEventListener('click', () => {
-    if ($('nav ul li').is(':visible') && window.innerWidth < 1077) {
-        $('nav ul li').slideToggle();
-        burger.classList.remove('active');
-    }
-});
-
-links.forEach((link) => {
-    link.addEventListener('click', () => {
-        if (window.innerWidth < 1077) {
-            burger.classList.remove('active');
+    hidden.addEventListener('click', () => {
+        if ($('nav ul li').is(':visible') && window.innerWidth < 1077) {
             $('nav ul li').slideToggle();
-            
+            burger.classList.remove('active');
         }
     });
-});
 
-window.addEventListener('scroll', () => {
-    let scrollValue =
-        (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.85 && playOnce) {
-        popup.style.opacity = 1;
-        popup.style.transform = 'none';
-        playOnce = false;
-    }
-});
+    links.forEach((link) => {
+        link.addEventListener('click', () => {
+            if (window.innerWidth < 1077) {
+                burger.classList.remove('active');
+                $('nav ul li').slideToggle();
+            }
+        });
+    });
 
-closeBtn.addEventListener('click', () => {
-    popup.style.opacity = 0;
-    popup.style.transform = 'translateX(500px)';
-});
+    window.addEventListener('scroll', () => {
+        let scrollValue =
+            (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+        if (scrollValue > 0.85 && playOnce) {
+            popup.style.opacity = 1;
+            popup.style.transform = 'none';
+            playOnce = false;
+        }
+    });
 
+    closeBtn.addEventListener('click', () => {
+        popup.style.opacity = 0;
+        popup.style.transform = 'translateX(500px)';
+    });
+})(jQuery);
